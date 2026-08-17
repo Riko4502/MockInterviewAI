ee194f33a740a49e243d5e98af65691f8d94f067
ontonomoot <obrv.igl@gmail.com>
2026-08-17 13:01:48 +0400

    chore: add storybook to packages/ui, fix missing tailwindcss import

---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+import { dirname } from "path"
+
+import { fileURLToPath } from "url"
+import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite"
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string) {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-vitest'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-onboarding')
+  ],
+  "framework": getAbsolutePath('@storybook/react-vite'),
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(tsconfigPaths());
+    return config;
+  },
+};
+export default config;
