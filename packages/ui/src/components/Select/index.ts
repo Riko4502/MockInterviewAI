acd342c9b7dd3f8bf060babc92329db6356c32a2
Dmitry <dmit.bystroff2016@yandex.ru>
2026-08-20 14:23:16 +0300

    MOC-19-feat: add select component

---
+++ packages/ui/src/components/Select/index.ts
@@ -0,0 +1,9 @@
+export {
+  Select,
+  selectTriggerVariants,
+  selectContentVariants,
+  type SelectProps,
+  type SelectTriggerProps,
+  type SelectContentProps,
+  type SelectVariant,
+} from "./select";
