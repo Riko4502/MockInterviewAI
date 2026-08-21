f314062c00655f27f4d892f2b1b93d97489dfcef
Sobaken9D <ddrevetnak@gmail.com>
2026-08-21 18:33:19 +0300

    fix(ui): изменил page в изначальное состояние  #MOC-21

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,37 +1,3 @@
-// export default function Home() {
-//   return <main>Mock Interview AI</main>;
-// }
-
-// import { Avatar, AvatarImage, AvatarFallback } from "@packages/ui";
-//
-// export default function Home() {
-//   return (
-//     <main className="p-10 flex items-center gap-4">
-//       <Avatar size="lg">
-//         <AvatarImage src="https://github.com/shadcn.png" alt="User Avatar" />
-//         <AvatarFallback>CN</AvatarFallback>
-//       </Avatar>
-//
-//       <Avatar size="md">
-//         <AvatarFallback>AI</AvatarFallback>
-//       </Avatar>
-//     </main>
-//   );
-// }
-
-import { Avatar } from "@packages/ui";
-
 export default function Home() {
-  return (
-    <main className="p-10 flex items-center gap-4">
-      <Avatar size="lg">
-        <Avatar.Image src="https://github.com/shadcn.png" alt="User Avatar" />
-        <Avatar.Fallback>CN</Avatar.Fallback>
-      </Avatar>
-
-      <Avatar size="md">
-        <Avatar.Fallback>AI</Avatar.Fallback>
-      </Avatar>
-    </main>
-  );
+  return <main>Mock Interview AI</main>;
 }
--- packages/ui/src/components/Avatar/avatar.tsx
+++ packages/ui/src/components/Avatar/avatar.tsx
@@ -45,4 +45,4 @@ export const Avatar = Object.assign(AvatarRoot, {
   Fallback: AvatarFallback,
 })
 
-export { AvatarImage, AvatarFallback }
+export {AvatarImage, AvatarFallback}
