c54012fdc4f91d19c12c59013a6fe61b947e2c29
Mikhail Ryzhikov <riko4502@gmail.com>
2026-08-21 22:36:40 +0300

    landing

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,6 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ru'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
