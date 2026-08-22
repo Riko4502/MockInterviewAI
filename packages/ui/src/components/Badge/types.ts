35f9a5c0123d0ef160eae790ef494d9582def66e
Sobaken9D <ddrevetnak@gmail.com>
2026-08-22 12:12:24 +0300

    feat(ui): add Badge component #MOC-21

---
+++ packages/ui/src/components/Badge/types.ts
@@ -0,0 +1,8 @@
+import * as React from "react"
+import {type VariantProps} from "class-variance-authority"
+import {badgeVariants} from "./constants"
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {
+}
