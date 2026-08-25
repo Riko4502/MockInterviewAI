8746859c21a059661c2af397468cc397cac265f2
Данилова Тамара Николаевна <tdanilova@corp.finam.ru>
2026-08-25 18:16:30 +0300

    chore(husky): добавить валидацию commit message через commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'style', 'chore', 'ci']],
+    'scope-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+};
+
