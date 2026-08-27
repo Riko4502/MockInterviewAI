48fd4a20131a2fc922012a5f2b2a39616e0313a4
Mikhail Ryzhikov <riko4502@gmail.com>
2026-08-27 14:36:48 +0300

    fix(ui): configure rslib build, path aliases and exports

--- packages/ui/rslib.config.ts
+++ packages/ui/rslib.config.ts
@@ -1,40 +1,26 @@
-import path from "node:path";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  lib: [
-    {
-      format: "esm",
-      output: {
-        distPath: {
-          root: "./dist",
-        },
-      },
-      dts: true,
-    },
-    {
-      format: "cjs",
-      output: {
-        distPath: {
-          root: "./dist",
-        },
-        minify: false,
-      },
-    },
-  ],
   source: {
     entry: {
-      index: "./src/index.ts",
-      rsbuild: "./rsbuild/index.ts",
-    },
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+      index: ["./src/**"],
     },
   },
+  lib: [
+    {
+      bundle: false,
+      format: "esm",
+      dts: true,
+    },
+  ],
   output: {
-    cleanDistPath: true,
-    target: "web",
+    copy: [
+      {
+        from: "./src/styles",
+        to: "./styles",
+      },
+    ],
   },
+  plugins: [pluginReact()],
 });
