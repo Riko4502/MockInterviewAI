3a02357bed7cdccfc5baaf91daaadf494d5edda1
Mikhail Ryzhikov <riko4502@gmail.com>
2026-08-28 16:06:01 +0300

    chore(repo): update lint file delete pr

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,9 +1,22 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'style', 'chore', 'ci']],
-    'scope-empty': [2, 'never'],
-    'subject-case': [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "refactor",
+        "perf",
+        "docs",
+        "test",
+        "style",
+        "chore",
+        "ci",
+      ],
+    ],
+    "scope-empty": [2, "never"],
+    "subject-case": [0],
   },
 };
-
