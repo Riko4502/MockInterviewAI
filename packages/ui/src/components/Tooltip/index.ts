3049a7075eb823a55787be31c8be2208e4d82d36
Mikhail Ryzhikov <riko4502@gmail.com>
2026-09-01 14:06:20 +0300

    feat(ui): добавить компонент Tooltip и истории в Storybook (MOC-46)

---
+++ packages/ui/src/components/Tooltip/index.ts
@@ -0,0 +1,10 @@
+export { TOOLTIP_STYLES } from "./constants";
+export { Tooltip } from "./tooltip";
+export type {
+  TooltipArrowProps,
+  TooltipContentProps,
+  TooltipProps,
+  TooltipProviderProps,
+  TooltipRootProps,
+  TooltipTriggerProps,
+} from "./types";
