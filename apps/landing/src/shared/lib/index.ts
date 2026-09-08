cc004b32360b16c018e42fc0ff91fc5cf429e760
Mikhail Ryzhikov <riko4502@gmail.com>
2026-09-08 23:58:53 +0300

    feat(i18n): export shared i18n utilities

--- apps/landing/src/shared/lib/index.ts
+++ apps/landing/src/shared/lib/index.ts
@@ -1,2 +1,7 @@
 export { default as i18n } from "./i18n";
+export { normalizeBrowserLocale } from "./i18n/browser-locale";
+export {
+  I18nClientProvider,
+  type I18nClientProviderProps,
+} from "./i18n/I18nClientProvider";
 export { useLandingTranslations } from "./use-landing-translations";
